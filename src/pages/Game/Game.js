bc11cf0eeca555922c064b2a71339d24ab5fe1eb
Carlos Alberto Fis Fernández <codebreaker198@gmail.com>
2023-03-02 13:32:56 -0600

    Styles the game page

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {
+	StyledGame,
+	StyledScore,
+	StyledTimer,
+	StyledCharacter,
+} from '../../styled/Game';
+
+import { Strong } from '../../styled/Random';
 
 export default function Game() {
 	return (
-		<div>
-			<h1>Game</h1>
-		</div>
+		<StyledGame>
+			<StyledScore>
+				Score: <Strong>0</Strong>
+			</StyledScore>
+			<StyledCharacter>A</StyledCharacter>
+			<StyledTimer>
+				Time: <Strong>00: 0000</Strong>
+			</StyledTimer>
+		</StyledGame>
 	);
 }
